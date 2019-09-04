a2d2f4f73229d38c92de2e6e955165792edcef27
Sven-Hendrik Haase <svenstaro@gmail.com>
2019-09-04 17:32:26 +0200

    Refactor stuff to fetch upx using package managers instead

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,17 +1,25 @@
 import * as fs from 'fs';
+import * as os from 'os';
+import * as child_process from 'child_process';
 import { run } from '../src/action';
 
-beforeEach(() => {
-    fs.unlink("upx/src/upx.out", err => {
-        if(err && err.code != 'ENOENT') {
-            throw err;
-        }
-    });
+beforeAll(() => {
+    if (os.type() == "Linux") {
+        child_process.execSync("wget -c https://github.com/svenstaro/proxyboi/releases/download/0.1.5/proxyboi-linux-amd64")
+        fs.chmodSync("proxyboi-linux-amd64", "755");
+        process.env["INPUT_FILE"] = "proxyboi-linux-amd64"
+    } else if (os.type() == "Darwin") {
+        child_process.execSync("wget -c https://github.com/svenstaro/proxyboi/releases/download/0.1.5/proxyboi-macos-amd64")
+        fs.chmodSync("proxyboi-macos-amd64", "755");
+        process.env["INPUT_FILE"] = "proxyboi-macos-amd64"
+    } else if (os.type() == "Windows_NT") {
+        child_process.execSync("wget -c https://github.com/svenstaro/proxyboi/releases/download/0.1.5/proxyboi-windows-amd64.exe")
+        process.env["INPUT_FILE"] = "proxyboi-windows-amd64.exe"
+    }
 });
 
 describe('UPX Action', () => {
-    it('can compress its own UPX', async () => {
-        process.env["INPUT_FILE"] = "upx/src/upx.out"
+    it('can compress stuff', async () => {
         await run();
     });
 });
