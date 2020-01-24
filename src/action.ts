2047eed681230d274d7b81792a350abe0b627924
Sven-Hendrik Haase <svenstaro@gmail.com>
2020-01-24 03:57:29 +0100

    Update to UPX 3.96

--- src/action.ts
+++ src/action.ts
@@ -2,25 +2,27 @@ import * as fs from 'fs';
 import * as os from 'os';
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
-import * as download from 'download';
+import download = require('download');
+import decompressTarxz = require('decompress-tarxz');
 import * as path from 'path';
 
 async function downloadUpx(): Promise<string> {
     const tmpdir = fs.mkdtempSync(path.join(os.tmpdir(), "upx-action-"));
     if (os.type() == "Linux") {
-        await download.default('https://github.com/upx/upx-automatic-builds/blob/devel-20200108-0f4975f-travis/amd64-linux-gcc-9-release/upx-git-0f4975fd7ffb.out?raw=true', tmpdir, { filename: "upx" });
-        const upx_path = `${tmpdir}/upx`;
+        await download('https://github.com/upx/upx/releases/download/v3.96/upx-3.96-amd64_linux.tar.xz', tmpdir, {
+            extract: true, plugins: [ decompressTarxz() ]
+        });
+        const upx_path = `${tmpdir}/upx-3.96-amd64_linux/upx`;
         fs.chmodSync(upx_path, "755");
         return upx_path;
     } else if (os.type() == "Darwin") {
-        await download.default('https://github.com/upx/upx-automatic-builds/blob/devel-20200108-0f4975f-travis/amd64-darwin-clang-1100-release/upx-git-0f4975fd7ffb.out?raw=true', tmpdir, { filename: "upx" });
-        const upx_path = `${tmpdir}/upx`;
-        fs.chmodSync(upx_path, "755");
-        return upx_path;
+        await exec.exec('brew install upx');
+        return "upx";
     } else if (os.type() == "Windows_NT") {
-        await download.default('https://github.com/upx/upx-automatic-builds/blob/devel-20200108-0f4975f-appveyor/amd64-win64-msvc-14.1-release/upx-git-0f4975fd7ffb.exe?raw=true', tmpdir, { filename: "upx.exe" });
-        const upx_path = `${tmpdir}/upx.exe`;
-        fs.chmodSync(upx_path, "755");
+        await download('https://github.com/upx/upx/releases/download/v3.96/upx-3.96-win64.zip', tmpdir, {
+            extract: true
+        });
+        const upx_path = `${tmpdir}/upx-3.96-win64/upx.exe`;
         return upx_path;
     }
     throw "unsupported OS";
@@ -42,7 +44,7 @@ export async function run() {
             await exec.exec(`strip ${strip_args} ${file}`);
         }
 
-        core.info('⬇️ Downloading UPX...');
+        core.info('⬇️  Downloading UPX...');
         const upx_path = await downloadUpx();
 
         core.info('🏃 Running UPX...');
